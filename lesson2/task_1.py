3d1e1cfe5dfc7e7e39be27c4c90ddcdfcd57aa6a
asteroidea <km560@mail.ru>
2022-02-05 02:46:04 +0300

    lesson2

---
+++ lesson2/task_1.py
@@ -0,0 +1,7 @@
+print("Task 1")
+
+catalog = ['Санкт-Петербург', 1, 3.4]
+
+print(catalog[0], " относится к типу ", type(catalog[0]))
+print(catalog[1], " относится к типу ", type(catalog[1]))
+print(catalog[-1], " относится к типу ", type(catalog[-1]))
