45d537e5fa729b8921464e21940044f195a959c6
asteroidea <km560@mail.ru>
2022-04-19 13:51:36 +0300

    update

---
+++ 0102_maths/lesson1/task_4.py
@@ -0,0 +1,13 @@
+from matplotlib.pyplot import (axes,axis,title,legend,figure,
+                               xlabel,ylabel,xticks,yticks,
+                               xscale,yscale,text,grid,
+                               plot,scatter,errorbar,hist,polar,
+                               contour,contourf,colorbar,clabel,
+                               imshow)
+from mpl_toolkits.mplot3d import Axes3D
+from numpy import (linspace,logspace,zeros,ones,outer,meshgrid,
+                   pi,sin,cos,sqrt,exp)
+from numpy.random import normal
+matplotlib inline
+
+plot([0,0.25,1],[0,1,0.5])
